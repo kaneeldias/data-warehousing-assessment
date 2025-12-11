9824ed9e6a05a7452b07521f1cd61df5a0d193e1
kaneeldias <kaneeldias@hotmail.com>
2025-12-11 10:49:32 +0530

    Add Dim_Product table and SSIS package for data transfer: create SQL table and configure SSIS package for data loading

---
+++ azure-sql-create-dim-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Dim_Product(
+    product_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    category VARCHAR(100)
+)
+GO
--- e5-employees.sql
+++ e5-employees.sql
@@ -1,8 +1,6 @@
-SELECT
-    e.employee_name AS 'Employee Name',
-    m.employee_name AS 'Manager Name'
+SELECT e.employee_name AS 'Employee Name',
+       m.employee_name AS 'Manager Name'
 FROM Employee e
-INNER JOIN
-    Employee m ON e.manager_id = m.employee_id
-ORDER BY
-    e.employee_id;
+        LEFT JOIN
+     Employee m ON e.manager_id = m.employee_id
+ORDER BY e.employee_id;
